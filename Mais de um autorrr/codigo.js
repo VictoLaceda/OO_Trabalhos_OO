2893af31266c0a68e1a7edad59540e42f5d5fadf
VictoLaceda <73712733+VictoLaceda@users.noreply.github.com>
2023-05-18 14:10:09 -0300

    TRABALHO FEITO

--- Mais de um autorrr/codigo.js
+++ Mais de um autorrr/codigo.js
@@ -1,20 +1,51 @@
-function leitura() {
+function pegaTags() {
     const elem = this.responseXML.documentElement;
     const livro = elem.getElementsByTagName('livro');
     console.log(livro);
 }
 
-function pegadir(elem, i, j) {
+function pegadirXML(elem, i, j) {
     const livro = elem.getElementsByTagName('livro')[i];
-    return [livro.getElementsByTagName("titulo")[j].firstChild.nodeValue, livro.getElementsByTagName("autor")[j].firstChild.nodeValue, livro.getElementsByTagName("ano")[j].firstChild.nodeValue, livro.getElementsByTagName("preco")[j].firstChild.nodeValue];
+    const titulo = livro.getElementsByTagName("titulo")[j].firstChild.nodeValue;
+    const autores = livro.getElementsByTagName("autor");
+    const autoresArray = [];
+
+    for (let k = 0; k < autores.length; k++) {
+        autoresArray.push(autores[k].firstChild.nodeValue);
+    }
+
+    const ano = livro.getElementsByTagName("ano")[j].firstChild.nodeValue;
+    const preco = livro.getElementsByTagName("preco")[j].firstChild.nodeValue;
+
+    return [titulo, autoresArray, ano, preco];
 }
 
 function jogaParaPagina() {
     const elem = this.responseXML.documentElement;
-    const matrizDiretorio = [];
+    const matrizXML = [];
+
     for (let i = 0; i < elem.getElementsByTagName('livro').length; i++) {
-        matrizDiretorio[i] = pegadir(elem, i, 0);
-        console.log(matrizDiretorio[i][1]);
+        const matrizDados = pegadirXML(elem, i, 0);
+        matrizXML.push(matrizDados);
+
+        const linhaDados = document.getElementById('linha_dados');
+        const clonado = linhaDados.cloneNode(true);
+
+        clonado.querySelector('td:nth-child(1)').textContent = matrizDados[0];
+
+        const autoresList = clonado.querySelector('ul');
+        autoresList.innerHTML = '';
+
+        matrizDados[1].forEach(function(autor) {
+            const li = document.createElement('li');
+            li.textContent = autor;
+            autoresList.appendChild(li);
+        });
+
+        clonado.querySelector('td:nth-child(3)').textContent = matrizDados[2];
+        clonado.querySelector('td:nth-child(4)').textContent = matrizDados[3];
+
+        linhaDados.parentNode.appendChild(clonado);
     }
 }
 
